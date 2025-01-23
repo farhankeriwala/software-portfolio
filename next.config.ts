b4c7c77110f3935ebcc110f25d206faf4f4950cb
farhankeriwala <114573891+farhankeriwala@users.noreply.github.com>
2025-01-23 18:11:41 +0000

    complete maintenance

--- next.config.ts
+++ next.config.ts
@@ -21,16 +21,16 @@ const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true, // Ignores ESLint build errors
     },
-
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/maintenance", // Redirects to the maintenance page
-                permanent: false, // Set to false for temporary redirects (e.g., during maintenance)
-            },
-        ];
-    },
+    //
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: "/",
+    //             destination: "/maintenance", // Redirects to the maintenance page
+    //             permanent: false, // Set to false for temporary redirects (e.g., during maintenance)
+    //         },
+    //     ];
+    // },
 };
 
 export default nextConfig;
