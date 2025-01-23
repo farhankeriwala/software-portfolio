d0ec6ffa15998d5b4de630ddf246f61aeeb7c8f1
farhankeriwala <114573891+farhankeriwala@users.noreply.github.com>
2025-01-23 17:47:40 +0000

    new footer for maintenance

--- app/maintenance/layout.tsx
+++ app/maintenance/layout.tsx
@@ -39,6 +39,12 @@ export default function RootLayout({
         className={`${lato.variable} ${poppins.variable} ${bricolageGrotesque.variable} flex flex-col min-h-screen antialiased`}
       >
         <main className="flex-grow container mx-auto">{children}</main>
+        <footer className={"container mb-12 mt-20"}>
+        <div
+            className={"flex flex-col-reverse md:flex-row items-center justify-center md:justify-between gap-y-4 py-8"}>
+            <p className={"font-poppins text-ld font-medium"}>© 2025 Farhan Keriwala. All rights reserved.</p>
+        </div>
+    </footer>
       </body>
     </html>
   );
