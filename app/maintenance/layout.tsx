a75a7c5ee40cac7827eec84916cff6e709d2bca5
farhankeriwala <114573891+farhankeriwala@users.noreply.github.com>
2025-01-23 17:45:06 +0000

    new maintenance layout

--- app/maintenance/layout.tsx
+++ app/maintenance/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Poppins, Lato, Bricolage_Grotesque } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
 const lato = Lato({
