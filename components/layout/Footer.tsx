aeadc78fcb2eb53cb6bc98eb7741a665e3858d4a
farhankeriwala <114573891+farhankeriwala@users.noreply.github.com>
2025-01-23 17:50:07 +0000

    new footer for maintenance

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,23 +1,27 @@
 import {socialMediaItems} from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Footer = () => {
+    const path = usePathname();
     return (<footer className={"container mb-12 mt-20"}>
         <div
             className={"flex flex-col-reverse md:flex-row items-center justify-center md:justify-between gap-y-4 py-8"}>
             <p className={"font-poppins text-ld font-medium"}>© 2025 Farhan Keriwala. All rights reserved.</p>
-            <div className={"flex items-center space-x-6 md:space-x-8"}>
-                {socialMediaItems.map((item: SocialMediaItem) => (<Link
-                    href={item.url}
-                    key={item.label}
-                    className={"hover:scale-105"}
-                    target="_blank"
-                >
-                    <Image src={item.icon} alt={item.label} width={28} height={28}/>
-                </Link>))}
-            </div>
+            {
+                path === '/maintenance' ? (<></>) : (<div className={"flex items-center space-x-6 md:space-x-8"}>
+                    {socialMediaItems.map((item: SocialMediaItem) => (<Link
+                        href={item.url}
+                        key={item.label}
+                        className={"hover:scale-105"}
+                        target="_blank"
+                    >
+                        <Image src={item.icon} alt={item.label} width={28} height={28}/>
+                    </Link>))}
+                </div>)
+            }
         </div>
     </footer>);
 };
