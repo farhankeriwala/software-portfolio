efdeba87f4ad0f6ea6eac6dcd124094665225e7f
farhankeriwala <114573891+farhankeriwala@users.noreply.github.com>
2025-02-05 13:30:47 +0000

    Start new Blog section tba

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,22 @@ import {heroCodeBlockCode} from "@/constants";
 import Stats from "@/components/Stats";
 import {CodeBlock} from "@/components/ui/aceternity/CodeBlock";
 import Introduction from "@/components/Introduction";
+import Blog from "@/components/blog/Blog";
 
 const Page: React.FC = () => {
 
     return (
-      <section className="flex flex-col items-center justify-center min-h-screen">
-        <CodeBlock
-          highlightLines={[9, 11, 13, 15, 17]}
-          language={"tsx"}
-          filename={"Home.tsx"}
-          code={heroCodeBlockCode}
-        />
-        <Stats />
-        <Introduction />        
-      </section>
+        <section className="flex flex-col items-center justify-center min-h-screen">
+            <CodeBlock
+                highlightLines={[9, 11, 13, 15, 17]}
+                language={"tsx"}
+                filename={"Home.tsx"}
+                code={heroCodeBlockCode}
+            />
+            <Stats/>
+            <Introduction/>
+            <Blog/>
+        </section>
     );
 };
 
